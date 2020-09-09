f97b662fe77df0984e26e86fdd3e0bfa48e3e321
FreakyTorial <maximiliangroiser@gmail.com>
2020-09-09 15:24:07 +0200

    Add StringHelper functions

--- src/huff/lib/helper/StringHelper.java
+++ src/huff/lib/helper/StringHelper.java
@@ -1,5 +1,7 @@
 package huff.lib.helper;
 
+import org.apache.commons.lang.Validate;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class StringHelper
@@ -26,12 +28,40 @@ public class StringHelper
 		return string != null && !string.trim().isEmpty();
 	}
 	
-	public static boolean isIn(boolean ignoreCase, String value, String... list)
+	public static boolean isIn(boolean ignoreCase, @NotNull String value, @NotNull String... list)
 	{
+		Validate.notNull((Object) value, "The string-value cannot be null.");
+		Validate.notNull((Object) list, "The string-list cannot be null.");
+		
 		for (String compareValue : list)
 		{
 			if (ignoreCase ? value.equalsIgnoreCase(compareValue) : value.equals(compareValue)) return true;
 		}
 		return false;
+	}
+
+	public static boolean contains(boolean ignoreCase, @NotNull String value, @NotNull String... list)
+	{
+		Validate.notNull((Object) value, "The string-value cannot be null.");
+		Validate.notNull((Object) list, "The string-list cannot be null.");
+		
+		for (String compareValue : list)
+		{
+			if (ignoreCase ? value.toLowerCase().contains(compareValue.toLowerCase()) : value.contains(compareValue)) return true;
+		}
+		return false;
+	}
+	
+	public static @NotNull String build(@NotNull Object... objects)
+	{
+		Validate.notNull(objects, "The build-part-objects cannot be null.");
+		
+		final StringBuilder builder = new StringBuilder();
+		
+		for (Object object : objects)
+		{
+			builder.append(object);
+		}
+		return builder.toString();
 	}
 }
