93de18efea286f9dabf3d2dc46cde6dfbd3b06d8
Max <maximiliangroiser@gmail.com>
2020-11-30 08:55:19 +0100

    Added JavaDoc comments

--- src/huff/lib/helper/StringHelper.java
+++ src/huff/lib/helper/StringHelper.java
@@ -8,26 +8,58 @@ public class StringHelper
 {
 	private StringHelper() { }
 	
-	public static boolean isNullOrEmpty(@Nullable String string)
+	/**
+	 * Checks if a string is null or empty.
+	 * 
+	 * @param   value   the string to check
+	 * @return          A boolean that gives the check result.
+	 */
+	public static boolean isNullOrEmpty(@Nullable String value)
 	{
-		return string == null || string.isEmpty();
+		return value == null || value.isEmpty();
+	}
+
+	/**
+	 * Checks if a string is null, empty or has only whitespaces.
+	 * 
+	 * @param   value   the string to check
+	 * @return          A boolean that gives the check result
+	 */
+	public static boolean isNullOrWhitespace(@Nullable String value)
+	{
+		return value == null || value.trim().isEmpty();
 	}
 	
-	public static boolean isNullOrWhitespace(@Nullable String string)
+	/**
+	 * Checks if a string is not null and empty.
+	 * 
+	 * @param   value   the string to check
+	 * @return          A boolean that gives the check result
+	 */
+	public static boolean isNotNullOrEmpty(@Nullable String value)
 	{
-		return string == null || string.trim().isEmpty();
+		return value != null && !value.isEmpty();
 	}
 	
-	public static boolean isNotNullOrEmpty(@Nullable String string)
-	{
-		return string != null && !string.isEmpty();
-	}
-	
-	public static boolean isNotNullOrWhitespace(@Nullable String string)
+	/**
+	 * Checks if a string is not null, not empty or don't has only whitespaces.
+	 * 
+	 * @param   value   the string to check
+	 * @return          A boolean that gives the check result
+	 */
+	public static boolean isNotNullOrWhitespace(@Nullable String value)
 	{
 		return string != null && !string.trim().isEmpty();
 	}
-	
+
+	/**
+	 * Checks if the string is equals a string from the list.
+	 * 
+	 * @param   ignoreCase   option to (de-)activate case sensitivity
+	 * @param   value   	 a string to compare
+	 * @param   list	 a list of strings to compare
+	 * @return           	 A boolean that gives the comparing result
+	 */
 	public static boolean isIn(boolean ignoreCase, @NotNull String value, @NotNull String... list)
 	{
 		Validate.notNull((Object) value, "The string-value cannot be null.");
@@ -40,6 +72,14 @@ public class StringHelper
 		return false;
 	}
 
+	/**
+	 * Checks if the string is contained in a string from the list.
+	 * 
+	 * @param   ignoreCase   option to (de-)activate case sensitivity
+	 * @param   value   	 a string to compare
+	 * @param   list	 a list of strings to compare
+	 * @return           	 A boolean that gives the comparing result
+	 */
 	public static boolean contains(boolean ignoreCase, @NotNull String value, @NotNull String... list)
 	{
 		Validate.notNull((Object) value, "The string-value cannot be null.");
@@ -51,7 +91,13 @@ public class StringHelper
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Builds all given objects with a StringBuilder to a string.
+	 * 
+	 * @param   objects   a list of object to build
+	 * @return            The string built out of the objects
+	 */
 	public static @NotNull String build(@NotNull Object... objects)
 	{
 		Validate.notNull(objects, "The build-part-objects cannot be null.");
