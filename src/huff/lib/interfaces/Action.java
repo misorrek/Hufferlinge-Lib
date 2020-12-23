f0906fc0aba60678cf8d4c40a8d564b6819a8e16
Max <maximiliangroiser@gmail.com>
2020-12-23 13:49:07 +0100

    Added javadoc comments

--- src/huff/lib/interfaces/Action.java
+++ src/huff/lib/interfaces/Action.java
@@ -2,6 +2,9 @@ package huff.lib.interfaces;
 
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * A util interface to pass a function with parameters that can be executed by the receiver.
+ */
 public interface Action
 {
 	public void execute(@Nullable Object... params);
