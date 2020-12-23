280a25a75d54ed0be4fb8d2b65a24289300e57cf
Max <maximiliangroiser@gmail.com>
2020-12-23 13:51:13 +0100

    Added javadoc comment

--- src/huff/lib/interfaces/RedisProperties.java
+++ src/huff/lib/interfaces/RedisProperties.java
@@ -2,8 +2,11 @@ package huff.lib.interfaces;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A data interface that specifies all important details for a redis connection.
+ */
 public interface RedisProperties
 {
-    public abstract @NotNull String getHost();
+	public abstract @NotNull String getHost();
 	public abstract int getPort();
 }
